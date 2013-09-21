edcf11a7fd10957c44cde28fcd79c47bf3e6b2be
Saif Mulla <saifmulla@saifmba.local>
2013-09-21 11:14:19 +0100

    first commit towards implementation of hybrid polyfoam for OF

---
+++ gpuPolyFoam.h
@@ -0,0 +1,13 @@
+/**
+ * //identity: $Id$
+ * filename: gpuPolyFoam.h
+ * @developer: Saif Mulla
+ * @created: 21/09/2013
+ * Application: gpuPolyFoam
+ * Description:
+ */
+
+#include "fvCFD"
+#include "mdPoly.H"
+#include "clockTimer.H"
+
